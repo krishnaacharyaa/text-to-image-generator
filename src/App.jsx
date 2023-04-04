d696e2be221fc4d810906b32953680953dc30fa6
krishna-official <krishnaacharya.official@gmail.com>
2023-04-04 15:28:19 +0530

    Phase 1 - updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,6 @@ function App() {
 	const [prompt, setPrompt] = useState("");
 	const [images, setImages] = useState([]);
 	const [progress, setProgress] = useState(0);
-	// const [inProgress, setInProgress] = useState(false);
 	const currentProgress = useRef();
 
 	useEffect(() => {
@@ -131,75 +130,116 @@ function App() {
 		setImages(result);
 	};
 	return (
-		<div>
-			<label>
-				Enter Prompt:
-				<input
-					type="text"
-					value={prompt}
-					onChange={(e) => setPrompt(e.target.value)}
-				/>
-			</label>
-			<button onClick={handleSubmit}>Submit</button>
-			<button onClick={handleRandom} type="submit">
-				Random
-			</button>
-			{/* {progress > 0 && progress <= 100 && (
-				<Progress value={progress} color="green" className="h-5" />
-			)} */}
-			{progress !== 0 && <Progress value={progress} />}
-			<div className="grid grid-cols-2 gap-4">
-				{images.map((image, index) => (
-					<img key={index} src={image} alt={`Image ${index}`} />
-				))}
-			</div>
+		// <div>
+		// 	<label>
+		// 		Enter Prompt:
+		// 		<input
+		// 			type="text"
+		// 			value={prompt}
+		// 			onChange={(e) => setPrompt(e.target.value)}
+		// 		/>
+		// 	</label>
+		// 	<button onClick={handleSubmit}>Submit</button>
+		// 	<button onClick={handleRandom} type="submit">
+		// 		Random
+		// 	</button>
+		// 	{progress !== 0 && <Progress value={progress} />}
+		// 	<div className="grid grid-cols-2 gap-4 h-54">
+		// 		{images.map((image, index) => (
+		// 			<img key={index} src={image} alt={`Image ${index}`} />
+		// 		))}
+		// 	</div>
 
-			<ToastContainer />
+		// 	<ToastContainer />
+		// </div>
+		<div class="flex min-h-screen w-full flex-col object-contain">
+			<div class="relative h-96 w-full">
+				<div class="absolute h-full w-full bg-[url('https://static.fotor.com/app/features/img/aiimage/background_image3.jpg')] bg-cover object-cover"></div>
+				<div class="extra absolute flex h-full  w-full flex-col items-center justify-center bg-gradient-to-tr from-yellow-300 to-green-400 bg-clip-text text-3xl md:text-5xl  lg:text-7xl font-bold text-transparent">
+					Text to Image with <br />
+					<hr />
+					AI Image Generator
+				</div>
+				<div class="absolute bottom-0 flex flex-col md:flex-row  w-full gap-2 bg-transparent px-8 md:py-8 md:px-28">
+					<input
+						class="flex-1 rounded-lg border md:px-3 w-full p-2 md:p-0 "
+						type="text"
+						placeholder="Describe what you want to see, Confused what to type ? select Suprise Me"
+					/>
+					<div class="flex gap-2 justify-center items-center">
+						<button class="rounded-lg bg-green-400 p-2">Generate</button>
+						<button class="rounded-lg border border-green-400 p-2 text-green-400">
+							Suprise Me
+						</button>
+					</div>
+				</div>
+			</div>
+			<div class="hidden h-96 md:flex flex-col justify-start items-start md:px-28 px-8 py-8 bg-gradient-to-b from-[#2e1216] bg-black w-full ">
+				<div class="flex flex-col md:flex-row rounded-lg md:h-72 md:w-full ">
+					<img
+						class="  md:h-72 md:w-72 "
+						src="https://th.bing.com/th/id/OIP.TwiIfaePJW6nAT1P4dp_cgHaHa?pid=ImgDet&rs=1"
+					></img>
+					<div class="bg-gray-400 bg-opacity-40 flex-1 flex flex-col p-8">
+						<div class="text-white text-xl md:text-2xl font-extrabold mb-4">
+							Pro Tip 💡
+						</div>
+						<div class="text-white md:text-xl mb-8 font-semibold">
+							Get crazy with your prompt
+						</div>
+						<div class="text-white text-xl md:text-2xl">
+							Try: "An astronaut resting on mars in a beach chair, vibrant
+							lighting, elegant, highly detailed, smooth, sharp focus,
+							illustration, beautiful, geometric"
+						</div>
+					</div>
+				</div>
+				<div class="text-white">Your image is being created by AI</div>
+			</div>
+			<div class=" py-8 text-center text-2xl text-white bg-black">
+				Featured Gallery
+			</div>
+			<div class="flex md:flex-row flex-col justify-evenly gap-4 bg-black md:px-28 py-4">
+				<div class="relative h-full md:w-1/3 px-8 md:px-0">
+					<img
+						src="https://th.bing.com/th/id/OIG.lQW2ATPf8sWgXG4Dl7dT?pid=ImgGn"
+						alt="Your Image"
+						class="h-72 w-full rounded-lg"
+					/>
+					<div class="absolute inset-x-0 bottom-0 bg-black bg-opacity-50 p-4">
+						<h2 class="font-bold text-white">
+							Panda bear baking a cake in a sunny kitchen, digital art
+						</h2>
+					</div>
+				</div>
+				<div class="relative h-full md:w-1/3 px-8 md:px-0">
+					<img
+						src="https://www.greataiprompts.com/wp-content/uploads/2023/01/4268126f-9359-4a75-859a-3977946214ae-683x1024.jpg.webp"
+						alt="Your Image"
+						class="h-72 w-full  rounded-lg object-cover"
+					/>
+
+					<div class="absolute inset-x-0 bottom-0 bg-black bg-opacity-50 p-4">
+						<h2 class="font-bold text-white">
+							Portrait of a beutiful young woman of 18 age
+						</h2>
+					</div>
+				</div>
+				<div class="relative h-full md:w-1/3 px-8 md:px-0">
+					<img
+						src="https://th.bing.com/th/id/OIG.WMtTCbVcZ7AzNjn1tVwW?pid=ImgGn"
+						alt="Your Image"
+						class="h-72 w-full rounded-lg"
+					/>
+					<div class="absolute inset-x-0 bottom-0 bg-black bg-opacity-50 p-4">
+						<h2 class="font-bold text-white">
+							Renaissance painting of an elephant in a tuxedo
+						</h2>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
-	// const [mytime, setMytime] = useState(0);
-
-	// useEffect(() => {
-	// 	// create a interval and get the id
-	// 	const myInterval = setInterval(() => {
-	// 		setMytime((prevTime) => prevTime + 1);
-	// 	}, 1000);
-	// 	// clear out the interval using the id when unmounting the component
-	// 	return () => clearInterval(myInterval);
-	// }, []);
-	// useEffect(()=>{
-	// 	if(mytime>=10){
-	// 		clearInterval()
-	// 	}
-	// },[mytime])
-
-	// return <div> {mytime} </div>;
-
-	// const [time, setTime] = useState(0);
-	// const currentTimer = useRef();
-	// useEffect(() => {
-	// 	return () => clearInterval(currentTimer.current);
-	// }, []);
-	// const startTimer = () => {
-	// 	currentTimer.current = setInterval(() => {
-	// 		setTime((prev) => prev + 1);
-	// 	}, 1000);
-	// };
-	// const stopTimer = () => {
-	// 	clearInterval(currentTimer.current);
-	// };
-	// const resetTimer = () => {
-	// 	clearInterval(currentTimer.current);
-	// 	setTime(0);
-	// };
-	// return (
-	// 	<div>
-	// 		<div>{time}</div>
-	// 		<button onClick={startTimer}>Start</button>
-	// 		<button onClick={stopTimer}>Stop</button>
-	// 		<button onClick={resetTimer}>Reset</button>
-	// 	</div>
-	// );
 }
 
 export default App;
